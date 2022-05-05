bc9b425f8a0457fd073a205e8cd26709825e86e5
techAJAY <ajay2681998@gmail.com>
2022-05-05 12:40:08 +0530

    inter doc

--- src/controller/interncontroller.js
+++ src/controller/interncontroller.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
-const internModel = require("../model/internModel");
+const collegeModel = require("../model/collegeModel")
+const internModel = require("../model/internModel")
+const validator = require('validator');
 
 
-const isvalid  = function(value){
-    if(typeof value ==='undefine'  || value=== null) return false
-    if(typeof value === 'string'   && value.trim().length===0) return false
+
+const isValid = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    if (typeof value === 'number'  ) return false
     return true;
 }
 
@@ -13,16 +16,85 @@ const isValidRequestBody = function (requestBody) {
 }
 
 
-const internCreate = function(req,res){
+
+const internCreate = async function (req, res) {
+    try {
+        let requestBody = req.body
+
+       // validation user entry or  req.body
+
+        if (!isValidRequestBody(requestBody)) {
+            res.status(400).send({ status: false, message: 'Invalid request parameters. Please provide intern  details' })
+            return
+        }
+
+        if (!isValid(requestBody.name)) {
+            res.status(400).send({ status: false, message: 'name is required' })
+            return
+        }
+
+        if (!isValid(requestBody.email)) {
+            res.status(400).send({ status: false, message: 'email is required' })
+            return
+        }
+        
+        if (!isValid(requestBody.mobile)) {
+            res.status(400).send({ status: false, message: 'mobile number is required' })
+            return
+        }
+
+        if (!isValid(requestBody.collegeName)) {
+            res.status(400).send({ status: false, message: 'college name  is required' })
+            return
+        }
+
+         // email unique validation
+
+        if (!(validator.isEmail(requestBody.email))) {
+            return res.status(400).send({ status: false, msg: 'enter valid email' })
+        }
+
+        // number unique validation
+
+        let check1 = requestBody.mobile
+        let check2 = (/[0-9]{10}/.test(requestBody.mobile))
+        if (!(check1.length === 10 && check2)) {
+            return res.status(400).send({ status: false, msg: 'enter valid number' })
+        }
+
+        // validate given college exist or not
+
+        const collegeId = await collegeModel.findOne({ name: requestBody.collegeName })
+        if (!collegeId) {
+            return res.status(400).send({ status: false, msg: 'college not found' })
+        }
+        if (collegeId.isDeleted === true) {
+            return res.status(400).send({ status: false, msg: "college is deleted" })
+        }
+
+        // email unique validation
+
+        let emailCheck = await internModel.findOne({email:requestBody.email})
+        if(emailCheck){
+        return   res.status(400).send({status:false,msg:"this email already exist"})
+        }
+
+        // number unique validation 
+
+        let mobileCheck = await internModel.findOne({mobile:requestBody.mobile})
+        if(mobileCheck){
+        return  res.status(400).send({status:false,msg:"this mobile number is already exist"})
+        }
 
 
-
-
-
-
-
-
-
+        req.body.collegeId=collegeId._id
+        const internCreate = await internModel.create(requestBody)
+        res.status(200).send({ status: true, data: internCreate })
+        
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+    }
 }
 
-module.exports.interCreate = internCreate
+
+module.exports.internCreate = internCreate;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const collegeController = require("../controller/collegeController")
-
+const internController = require("../controller/internController")
 
 
 router.post("/createCollege",collegeController.collegeCreate)
+
+router.post("/createintern",internController.internCreate)
 
 
 module.exports = router;
